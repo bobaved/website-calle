ff031886b865eff889faf8062fa9c8b999fc9631
BobaVeD <leonard.kriese@votum.de>
2018-11-05 14:57:07 +0100

    [TASK] load music paths in nuxtServerInit

--- nuxt.config.js
+++ nuxt.config.js
@@ -93,6 +93,7 @@ module.exports = {
           exclude: /(node_modules)/
         })
       }
+      config.node = { fs: 'empty' }
     }
   }
 }
--- store/index.js
+++ store/index.js
@@ -1,5 +1,4 @@
 import Vuex from 'vuex'
-
 const createStore = () => {
   return new Vuex.Store({
     state: () => ({
@@ -12,7 +11,10 @@ const createStore = () => {
     },
     actions: {
       nuxtServerInit({ commit }) {
-        commit('setMusicPaths', ['lala/lala', 'bebe/bebe'])
+        const fs = require('fs-extra')
+        let path = 'content/music-list'
+        let musicList = fs.readdirSync(path)
+        commit('setMusicPaths', musicList.map(item => path + '/' + item))
       }
     }
   })
